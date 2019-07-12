ee4ab86fb85b69b07e46543b71b3a6868dc1a36d
kamillle <yuji.kmjm@gmail.com>
2019-07-13 01:17:35 +0900

    prepare AS.on_load hook

--- lib/detektiiv.rb
+++ lib/detektiiv.rb
@@ -1,5 +1,6 @@
 require "detektiiv/version"
 require "detektiiv/configuration"
+require "active_support/lazy_load_hooks"
 
 module Detektiiv
   class << self
@@ -13,4 +14,9 @@ module Detektiiv
       @configuration ||= Detektiiv::Configuration.new
     end
   end
+end
+
+ActiveSupport.on_load :factory_bot do
+  # TODO: このblock内でfactory_botにパッチを当てる
+  #       パッチ自体は別のクラスに隠蔽する
 end
