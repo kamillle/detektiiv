dc2060b9cdc239d050fafc1ac75e9e39a1616d6b
kamillle <yuji.kmjm@gmail.com>
2019-07-13 04:54:29 +0900

    fix indent

--- lib/detektiiv/factory_runner_patch.rb
+++ lib/detektiiv/factory_runner_patch.rb
@@ -10,7 +10,12 @@ module Detektiiv
     private
 
     def logging
-      factory_defined_filename = FactoryBot.factory_by_name(@name).instance_variable_get(:@definition).instance_variable_get(:@declarations).instance_variable_get(:@declarations)[0].instance_variable_get(:@block)
+      factory_defined_filename = FactoryBot
+                                  .factory_by_name(@name)
+                                  .instance_variable_get(:@definition)
+                                  .instance_variable_get(:@declarations)
+                                  .instance_variable_get(:@declarations)[0]
+                                  .instance_variable_get(:@block)
 
 
       if factory_defined_filename&.to_s&.include?("#{Detektiiv.configuration.application_name}/spec/factories") || factory_defined_filename&.to_s&.include?('factory_bot') || factory_defined_filename.nil?
