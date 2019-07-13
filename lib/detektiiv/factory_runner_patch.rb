adbcededc99f154c724e5bfafa596c3e254ffb21
kamillle <yuji.kmjm@gmail.com>
2019-07-14 02:01:09 +0900

    logging trait name and trait defined path

--- lib/detektiiv/factory_runner_patch.rb
+++ lib/detektiiv/factory_runner_patch.rb
@@ -18,13 +18,28 @@ module Detektiiv
                                   .instance_variable_get(:@declarations)[0]
                                   .instance_variable_get(:@block)
 
+      trait_defined_filenames = @traits.map { |trait_name|
+                                  FactoryBot
+                                    .factory_by_name(@name)
+                                    .instance_variable_get(:@definition)
+                                    .instance_variable_get(:@defined_traits)
+                                    .instance_variable_get(:@hash)
+                                    .select { |i| i.instance_variable_get(:@name).to_s.include?(trait_name.to_s) }.first.first
+                                }
 
-      if factory_defined_filename&.to_s&.include?("#{Detektiiv.configuration.application_name}/spec/factories") || factory_defined_filename&.to_s&.include?('factory_bot') || factory_defined_filename.nil?
+      # return if factory_defined_filename.nil? || factory_defined_filename.to_s.include?("#{Detektiiv.configuration.application_name}/spec/factories")
+
+      if factory_defined_filename.to_s.include?('definition_proxy')
+        trait_defined_filenames.each do |trait_info|
+          logger.debug(trait_name: trait_info.instance_variable_get(:@name), trait_defined_path: trait_info.instance_variable_get(:@block))
+        end
+        logger.debug(caller: caller.select { |i| i.include?("#{Detektiiv.configuration.application_name}/spec") })
+
         return
-      else
-        logger.debug(filepath: factory_defined_filename)
-        logger.debug(caller: caller.select {|i| i.include?("#{Detektiiv.configuration.application_name}/spec") }.to_s)
       end
+
+      logger.debug(filepath: factory_defined_filename)
+      logger.debug(caller: caller.select { |i| i.include?("#{Detektiiv.configuration.application_name}/spec") })
     end
 
     def logger
